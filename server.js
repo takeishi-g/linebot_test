c627ea1939d915768832c3400ccf5cede40d0149
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-11-19 11:54:48 +0900

    返信機能実装

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 'use strict'
 
 const express = require('express')
-const app = express()
-const port = 3000
+const line = require('@line/bot-sdk')
+const PORT = process.env.PORT || 3000
+require('dotenv').config()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken:process.env.CHANNEL_ACCESS_TOKEN
+}
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken:process.env.CHANNEL_ACCESS_TOKEN
+})
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello LINE BOT!'))
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
+})
+
+
+async function handleEvent(event) {
+  console.log(event)
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null)
+  }
+
+  const echo = { type: 'text', text: event.message.text }
+
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [echo]
+  })
+}
+
+app.listen(PORT, () => console.log(`Server ruuning at ${PORT}`))
